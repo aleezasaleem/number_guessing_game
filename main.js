1547aa4539174affc15eac3d64c02f6a39a0960c
Aleeza Saleem <150428514+aleezasaleem@users.noreply.github.com>
2023-12-25 12:20:51 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+import inquirer from "inquirer";
+let num = Math.floor(Math.random() * 10) + 1;
+for (let i = 0; i < 3; i++) {
+    let game = await inquirer.prompt({
+        type: "input",
+        name: "userguess",
+        message: "guess your number"
+    });
+    if (game.userguess === num) {
+        console.log("true answer");
+    }
+    else {
+        console.log("wrong guess");
+    }
+}
